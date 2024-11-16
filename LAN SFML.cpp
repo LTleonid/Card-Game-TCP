18057e00779696814fbfad0be13a07cd7a936185
LT_leonid <klayray10568@gmail.com>
2024-11-16 20:09:38 +0700

    Чиню дату, пробую с sfml для git

--- LAN SFML.cpp
+++ LAN SFML.cpp
@@ -1,10 +1,69 @@
 ﻿#include <iostream>
 #include <vector>
+#include <set>
 #include <SFML/Network.hpp>
 using namespace std;
 
+#pragma warning(disable : 4099)
+
 // Содержит JACK, QUEEN, KING, ACE, JOCKER
 enum suite { JACK, QUEEN, KING, ACE, JOCKER };
+enum Type { place = 0, accusation = 1 };
+
+struct Data {
+    int type;
+    bool accusation;
+    vector<int> cards;
+   
+    Data(int type, bool lie) : type{ type }, accusation{ lie } {}
+    Data(int type, vector<int> cards) : type{ type }, cards{ cards } {}
+    sf::Packet get() {
+        sf::Packet p;
+        if (type == Type::accusation) {
+            p << type << accusation;
+            cout << "Packet created for accusation: type = " << type
+                << ", accusation = " << accusation << endl;
+        }
+        else if (type == Type::place) {
+            p << type << cards.size();
+            cout << "Packet created for place: type = " << type
+                << ", cards = ";
+            for (int card : cards) {
+                cout << card << " ";
+                p << card;
+                cout << p;
+            }
+            cout << endl;
+        }
+        else {
+            cout << "Error: Undefined Type Data" << endl;
+            return sf::Packet();
+        }
+        return p;
+    }
+
+};
+
+sf::Packet& operator <<(sf::Packet& packet, const vector<int>& d) {
+    int size = d.size();
+    packet << size;
+    for (int value : d) {
+        packet << value;
+    }
+    return packet;
+}
+
+sf::Packet& operator >>(sf::Packet& packet, vector<int>& d) {
+    int size;
+    packet >> size;
+    for (int i = 0; i < size; ++i) {
+        int value;
+        packet >> value;
+        d.push_back(value);
+    }
+    return packet;
+}
+
 
 // Разменование карт по индексу
 string cardName(int cardIndex) {
@@ -68,12 +127,14 @@ public:
             return;
         cout << "Connected to server " << ip << endl;
     }
-
+    
     // Отправка данных на сервер
-    int sendData(string data, sf::TcpSocket& socket) {
-        if (data.empty()) { return -1; }
-        if (socket.send(data.c_str(), data.length()) == sf::Socket::Done) {
-            cout << "Data sent: " << data << endl;
+    int sendData(Data data, sf::TcpSocket& socket) {
+
+        sf::Packet p = data.get();
+        
+        if (socket.send(p) == sf::Socket::Done) {
+            cout << "Data sent" << endl;
             return 0;
         }
         return -2;
@@ -84,7 +145,7 @@ public:
 class Server : public Player {
 public:
     Server(string name, int uid) : Player(name, uid) {}
-
+    vector<int> deck;
     void startServer() {
         sf::TcpListener listener;
         if (listener.listen(53000) != sf::Socket::Done) {
@@ -93,7 +154,6 @@ public:
         }
         cout << "Server is listening on port 53000" << endl;
 
-        // Принятие входящего соединения
         sf::TcpSocket client;
         if (listener.accept(client) != sf::Socket::Done) {
             cout << "Error: Failed to accept client." << endl;
@@ -102,40 +162,52 @@ public:
 
         cout << "Client connected." << endl;
 
-        // Чтение данных от клиента
-        char buffer[128];
-        std::size_t received;
-        if (client.receive(buffer, sizeof(buffer), received) == sf::Socket::Done) {
-            cout << "Received data: " << string(buffer, received) << endl;
+        sf::Packet packet;
+        if (client.receive(packet) == sf::Socket::Done) {
+            int type;
+            packet >> type;
+            if (type == Type::accusation) {
+                bool lie;
+                packet >> lie;
+                cout << "Accusation received: " << (lie ? "True" : "False") << endl;
+            }
+            if (type == Type::place) {
+                int quanity;
+                packet >> quanity;
+                packet >> deck;    
+                cout << "Received cards: ";
+                for (int card : deck) {
+                    cout << card << " ";
+                }
+                cout << endl;
+            }
+
+        }
+        else {
+            cout << "Error: Failed to receive data." << endl;
         }
     }
+
 };
 
 int main() {
+    
     int u;
+    cout << "Enter mode (1 - Client 2 - Server): ";
     cin >> u;
     if (u == 1) {
         sf::TcpSocket socket;
         Player p("PlayerName", 123);
         p.connectServer(sf::IpAddress::getLocalAddress(), 53000, socket);
-        // Пример отправки данных
-        string data = "|123|1|0|0|0|0|0|";
-        int result = p.sendData(data, socket);
-        if (result == 0) {
-            cout << "Data sent successfully." << endl;
-        }
-        else {
-            cout << "Error sending data." << endl;
-        }
-
-        // Пример подключения к серверу
-
+        vector<int> cards{ 1,2,3 };
+        Data data(Type::place, cards);
+        p.sendData(data, socket);
     }
-    else {
-        // Пример создания сервера
+    else if (u == 2) {
         Server s("Server", 1);
         s.startServer();
     }
     cin >> u;
     return 0;
 }
+
